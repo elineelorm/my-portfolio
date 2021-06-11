a49077d1517f4ed6093d6f7e71423528dec21251
Eline <elinenuviadenu38@gmail.com>
2021-06-11 19:33:25 +0000

    Update with random message from server

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,6 +1,12 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+import com.google.gson.Gson;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,11 +14,37 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+public final class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("If at first you do not suceed, try again!");
+    
+    ArrayList<String> motivations = new ArrayList<String>(Arrays.asList("If at first you do not succeed, try again!", 
+  "Yes you can!", "If it was easy, everyone would do it!"));
+
+    String string = getRandom(motivations);
+
+    String json = convertToJson(string);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+   private String convertToJson(String motivations) {
+    String json = "[";
+    json += "\"" + motivations + "\"";
+    json += "]";
+    return json;
+  }
+
+  private String convertToJsonUsingGson(ArrayList<String> motivations) {
+    Gson gson = new Gson();
+    String json = gson.toJson(motivations);
+    return json;
+  }
+
+  private String getRandom(ArrayList<String> motivations){
+      Random rand = new Random();
+      return motivations.get(rand.nextInt(motivations.size()));
   }
 }
