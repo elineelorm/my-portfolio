13acb9fdc29ca27e616244e8cfc14dcd6427bc02
Eline <elinenuviadenu38@gmail.com>
2021-06-13 23:14:19 +0000

    Updated with Code Review comments

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -9,17 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
+  private static final String REDIRECT_URL = "https://enuviadenu-sps-summer21.uc.r.appspot.com/index.html";
+
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
 
-    // Print the value so you can see it in the server logs.
     System.out.println("You submitted: " + textValue);
 
-    // Write the value to the response so the user can see it.
     response.getWriter().println("You submitted: " + textValue);
-    response.sendRedirect("https://enuviadenu-sps-summer21.uc.r.appspot.com/index.html");
+    response.sendRedirect(REDIRECT_URL);
   }
 }
